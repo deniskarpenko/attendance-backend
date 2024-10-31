b788ab4bd93cad99cb8ada73556e9ac6ac7e40ae
Denys Karpenko <denisdataforsep@gmail.com>
2024-10-30 20:27:59 -0700

    add env.testing

--- tests/Unit/Auth/UserRepositoryTest.php
+++ tests/Unit/Auth/UserRepositoryTest.php
@@ -4,15 +4,36 @@ declare(strict_types=1);
 namespace Auth;
 
 use App\Auth\Domain\User;
+use App\Auth\Domain\ValueObjects\Email;
 use App\Auth\Insfrastructure\Persistence\Eloquent\UserRepository;
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
 
 class UserRepositoryTest extends TestCase
 {
+    use WithFaker;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->setUpFaker();
+    }
+
+    /**
+     * @return void
+     * @throws \App\Auth\Domain\Exceptions\InvalidValueObjectValueException
+     */
     public function testCreateUser(): void
     {
+        $email = new Email($this->faker->email());
+
+        $name = $this->faker->name();
+
+        $password = $this->faker->password();
+
         $userRepository = new UserRepository();
-        //$user = $userRepository->createUser();
+
+        $user = $userRepository->createUser($email, $password, $name);
         $this->assertTrue(true);
     }
 }
